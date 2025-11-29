d6951b38604295c27193538dc9f2d8360f9f2304
xtayzy <nursultantayteldiev@gmail.com>
2025-11-29 21:40:12 +0500

    Fix: Enable media file serving in production

--- back/unicrewback/urls.py
+++ back/unicrewback/urls.py
@@ -43,5 +43,5 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Обслуживание media файлов (работает и в production через Caddy)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
